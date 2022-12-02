ad142e6b1c983805e637cd80721c7f852d8d5683
LittleLebowski <o.f.atas@gmail.com>
2022-12-02 15:19:51 +0300

    Completed Assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,16 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MersaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+  console.log(RMFunctions);
+  return (
+    <div>
+      <MersaVerde />
+    </div>
+  );
 }
+
+export default ColoradoStateParks;
